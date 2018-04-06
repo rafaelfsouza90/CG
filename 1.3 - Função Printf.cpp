80ba734d8e685da7d195829f6e9b31a10e5e8872
Rafael Souza <37389147+rafaelfsouza90@users.noreply.github.com>
2018-04-06 13:32:12 -0300

    Printf Function
    Printf Function

---
+++ 1.3 - Função Printf.cpp
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+
+main(){
+	
+	int a = 1;
+	char tipo = 'c';
+	float valor = 22.53;
+	
+	printf("Inteiro: %d\nChar: %c\nFloat: %f\n", a, tipo, valor);
+	
+	//printf() - é de fato uma função asssim como a main
+	// () - Sentença a ser apresentada na tela e deve ser limitada entre (" ") 
+	
+	/*
+	O simbulo % dentro da sentença printf, indica que desejamos imprimir o conteúdo de alguma variável
+	
+	%c - Caracter
+	%s - String
+	%d - Inteiro
+	%f - Float (notação decimal)
+	%e - Float (notação exponencial)
+	%g - Float
+	%u - Inteiro sem sinal
+	%x - Hexadecinal inteiro
+	%o - Octal inteiro
+	%ld - Long
+	%lu - Long
+	%lx - Long
+	%lo - Long
+	
+	*/
+	
+	/* Existem outros caracteres de controle e todos inicial com um \ "
+	
+	\n - Nova linha
+	\t - Tabulação
+	\b - Backspace
+	\' - Aspas simples
+	\" - dupla aspas
+	\\ - Barra
+}
