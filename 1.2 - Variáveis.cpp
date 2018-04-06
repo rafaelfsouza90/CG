d77c4dff186b131894d9d7ef73ac2263c9029b0b
Rafael Souza <37389147+rafaelfsouza90@users.noreply.github.com>
2018-04-06 12:40:29 -0300

    Variables
    Variables

---
+++ 1.2 - Variáveis.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+main(){
+	
+	//Variável é um espaço reservado na memória do computador para armazenar certos tipos de dados
+	int num; //int - tipo da váriavel (inteiro) ; num - Nome da variável
+	
+	num = 2; //Atribui valor 2 a variável num
+	printf("Numero = %d\n", num);
+	
+	//Tipos de variáveis existentes:
+	//char - Capaz de conter um caractere (-128 a 127)
+	//short - Inteiro com capacidade reduzida (0 a 15384)
+	//int - Inteiro (-32768 a 32767) 
+	//long int - Inteiro estendido (-2 147 483 648 a 2 147 483 647)
+	//float - Ponto flutuante em precisão simples (10e-38 a 10e38)
+	//double - Ponto flutuante em precisão dupla (10e-308 a 10e308)
+	//unsigned char, int, long - Retira o sinal e portanto troca a variação de valores, por exemplo: unsigned int varia de 0 a 65 535
+	
+	
+}
