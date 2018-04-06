00add0707ee434ab365c3c3c584cdaca1c64dd25
Rafael Souza <37389147+rafaelfsouza90@users.noreply.github.com>
2018-04-06 12:21:41 -0300

    Introduction
    Introduction

---
+++ 1.1 - Introducao.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+main(){ 
+		
+		printf("Primeiro programa!"); 
+		
+		//main - consiste em uma função  
+		// { } - chaves delimitam o inicio e fim da função
+		//printf - imprime mensagens na tela padrão e conteúdo de variáveis
+	
+}
